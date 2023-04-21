1d454d0510701ca77709975b47b21b6fca72fe24
German Cabrera <c.germanjesus@gmail.com>
2023-04-21 00:44:50 -0300

    update4

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,93 +2,115 @@ import { useContext } from "react"
 import { CartContext } from "../../Context/CartContext"
 import { useState } from "react"
 import { Navigate } from "react-router-dom"
-import { collection, addDoc } from "firebase/firestore"
+import { collection, addDoc, query, where, documentId, writeBatch, getDocs } from "firebase/firestore"
 import { db } from "../../Firebase/Firebase"
 
 export const Checkout = () => {
-
     const { cart, totalCarrito, vaciarCarrito } = useContext(CartContext)
-    const [orderId, setOrderId] = useState(null)
 
+    const [orderId, setOrderId] = useState(null)
     const [values, setValues] = useState({
-        nombres: '',
-        apellido: '',
+        nombre: '',
         direccion: '',
         email: ''
-
-    }
-    )
+    })
 
     const handleInputChange = (e) => {
         setValues({
             ...values,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
+    
         const order = {
-            comprador: values,
+            cliente: values,
             items: cart,
             total: totalCarrito()
         }
+
+        const batch = writeBatch(db)
+
         const ordersRef = collection(db, "orders")
-        addDoc(ordersRef, order)
-            .then((doc) => {
-                setOrderId(doc.id)
-                vaciarCarrito()
-            })
+        const produtosRef = collection(db, "productos")
+        const q = query(produtosRef, where(documentId(), "in", cart.map(item => item.id)))
+
+        const outOfStock = []
+
+        const productos = await getDocs(q)
+
+        productos.docs.forEach((doc) => {
+            const item = cart.find((prod) => prod.id === doc.id)
+
+            if (doc.data().stock >= item.cantidad) {
+                batch.update(doc.ref, {
+                    stock: doc.data().stock - item.cantidad
+                })
+            } else {
+                outOfStock.push(item)
+            }
+        })
+
+        if (outOfStock.length === 0) {
+            await batch.commit()
+            const { id } = await addDoc(ordersRef, order)
+            setOrderId(id)
+            vaciarCarrito()
+
+        } else {
+            alert("Hay items sin stock: " + outOfStock.map(i => i.name).join(', '))
+        }
     }
 
     if (orderId) {
         return (
-            <div>
-                <h3>Tu compra fue registrada correctamente</h3>
-                <p>Tu numero de orden es: <strong>{orderId}</strong></p>
+            <div className="container my-5">
+                <h2>Tu compra se registró exitosamente!</h2>
+                <hr/>
+                <p>Guardá tu número de orden: <strong>{orderId}</strong></p>
             </div>
         )
     }
 
     if (cart.length === 0) {
-        return <Navigate to={"/"}/>
+        return <Navigate to="/"/>
     }
 
     return (
-        <div>
-            <h3> Completa el formulario</h3>
+        <div className="container my-5">
+            <h2>Ingresa tus datos</h2>
+            <hr/>
 
             <form onSubmit={handleSubmit}>
                 <input
-                type="text"
-                className="form control my-2"
-                placeholder="Nombres"
-                onChange={handleInputChange}
-                name="nombres"
+                    value={values.nombre}
+                    type="text"
+                    className="form-control my-2"
+                    placeholder="Tu nombre"
+                    onChange={handleInputChange}
+                    name="nombre"
                 />
-                   <input
-                type="text"
-                className="form control my-2"
-                placeholder="Apellido"
-                onChange={handleInputChange}
-                name="apellido"
+                <input
+                    value={values.direccion}
+                    type="text"
+                    className="form-control my-2"
+                    placeholder="Tu direccion"
+                    name="direccion"
+                    onChange={handleInputChange}
                 />
-                   <input
-                type="text"
-                className="form control my-2"
-                placeholder="Direccion"
-                onChange={handleInputChange}
-                name="direccion"
+                <input
+                    value={values.email}
+                    type="email"
+                    className="form-control my-2"
+                    placeholder="Tu email"
+                    name="email"
+                    onChange={handleInputChange}
                 />
-                   <input
-                type="email"
-                className="form control my-2"
-                placeholder="Email"
-                onChange={handleInputChange}
-                name="email"
-                />
+
+                <button className="btn btn-primary" type="submit">Enviar</button>
             </form>
-            <button className="btn-d" type="submit">Enviar</button>
         </div>
     )
 }
